883a3204098fe313e7750ffc032d53e69d864a74
DavidCPG <videdavid@hotmail.com>
2023-02-14 00:15:42 +0100

    [main]: Fixing start/setup bat files and adding powertoys config

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -1,16 +1,22 @@
 @REM Installing scoop, the 'non' command-line installer for Windows
-@REM powershell iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
-@REM scoop update
+echo ====== Installing Scoop and stuff =======
+powershell iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+powershell scoop update
+powershell scoop install neofetch
+powershell scoop install curl
 
-@REM Installing scoop programs
-@REM scoop install neofetch
-@REM scoop install curl
+@REM Installing Oh-my-posh -> Oh-my-zsh
+echo ====== Installing Oh-my-posh -> Oh-my-zsh (Linux) =======
+powershell Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
 
-@REM Installing winget programs
+@REM Installing PowerToys
+echo ====== Installing PowerToys =======
 powershell winget install Microsoft.PowerToys --source winget
 
 @REM Installing tilling windo manager
-powershell curl -o .\GlazeWM\GlazeWM.exe https://github.com/lars-berger/GlazeWM/releases/download/v1.11.1/GlazeWM_x64_1.11.1.exe
+echo ====== Installing GlazeWM (tilling window manager) =======
+powershell curl -o %USERPROFILE%\windotfiles\GlazeWM\GlazeWM.exe https://github.com/lars-berger/GlazeWM/releases/download/v1.11.1/GlazeWM_x64_1.11.1.exe
 
 @REM Pasting the start bat to the shell:startup
-powershell cp .\start.bat '%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\' 
+echo ====== Copying the start.bat file to the windows startup =======
+powershell cp %USERPROFILE%\windotfiles\scripts\start.bat '%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\'
