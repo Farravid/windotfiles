c0f769c1f564b126f3b5a27326dede3f1b2db1af
DavidCPG <videdavid@hotmail.com>
2023-02-14 01:52:22 +0100

    [main]: Powershell config and wallpapers

---
+++ Powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,2 @@
+# Init oh-my-posh with the gruvbox
+oh-my-posh init pwsh --config ~/jandedobbeleer.omp.json | Invoke-Expression
--- scripts/setup.bat
+++ scripts/setup.bat
@@ -1,13 +1,18 @@
 @REM Installing scoop, the 'non' command-line installer for Windows
 echo ====== Installing Scoop and stuff =======
-powershell iex (new-object net.webclient).downloadstring('https://get.scoop.sh')
+powershell irm get.scoop.sh -outfile 'install.ps1'
+powershell .\install.ps1 -RunAsAdmin
 powershell scoop update
 powershell scoop install neofetch
 powershell scoop install curl
+powershell rm %USERPROFILE%\windotfiles\install.ps1
 
-@REM Installing Oh-my-posh -> Oh-my-zsh
-echo ====== Installing Oh-my-posh -> Oh-my-zsh (Linux) =======
+@REM Installing Oh-my-posh
+echo ====== Installing Oh-my-posh =======
 powershell Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://ohmyposh.dev/install.ps1'))
+echo ====== Remember to install FuraMono font =======
+powershell oh-my-posh font install
+
 
 @REM Installing PowerToys
 echo ====== Installing PowerToys =======
@@ -20,3 +25,9 @@ powershell curl -o %USERPROFILE%\windotfiles\GlazeWM\GlazeWM.exe https://github.
 @REM Pasting the start bat to the shell:startup
 echo ====== Copying the start.bat file to the windows startup =======
 powershell cp %USERPROFILE%\windotfiles\scripts\start.bat '%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\'
+
+@REM Creating the powershell profile
+echo ====== Replacing the PowerShell profile =======
+powershell cp %USERPROFILE%\windotfiles\PowerShell\Microsoft.PowerShell_profile.ps1 $PROFILE
+
+@REM TODO: powershell settings for font 
